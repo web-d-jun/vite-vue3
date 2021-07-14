6381e9d757580f0c97fd4e8d2cd89974c9c5fb8c
junyoung <cjy874545@gamil.com>
2021-07-15 00:38:03 +0900

    update

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,21 +1,30 @@
 <template>
-    <div id="header">
+    <div id="header" class="flex">
         <!-- <app-button shape="round">
             <template #icon></template>
         </app-button> -->
         <div class="icon__container"><IconBars /></div>
-        <div class="switch__container">
-          <AppSwitch :active="active" @click="active = !active"/>
+        <div class="switch__container flex align-center">
+            <div
+                :class="[
+                    'dark-bright__wrap',
+                    { dark: active, bright: !active },
+                ]"
+            >
+                <div><font-awesome-icon icon="sun"></font-awesome-icon></div>
+                <div><font-awesome-icon icon="moon"></font-awesome-icon></div>
+            </div>
+
+            <AppSwitch :active="active" @click="active = !active" />
         </div>
     </div>
 </template>
 <script setup>
-import {ref} from  'vue';
+import { ref } from 'vue'
 import AppButton from 'components/AppButton.vue'
 import IconBars from 'icons/Bars.vue'
 import AppSwitch from 'components/AppSwitch.vue'
 const active = ref(false)
-
 </script>
 <style lang="scss" scoped>
 #header {
@@ -25,5 +34,31 @@ const active = ref(false)
     display: flex;
     align-items: center;
     position: relative;
+    padding: 0 10px;
+    box-sizing: border-box;
+    .icon__container {
+        flex: 1;
+    }
+    .switch__container {
+        height: 20px;
+        overflow: hidden;
+
+        & .dark-bright__wrap {
+            &.bright {
+                top: 10px;
+            }
+            &.dark {
+                top: -10px;
+            }
+            position: relative;
+
+            padding-right: 10px;
+            font-size: 15px;
+            -webkit-transition: all 0.4s ease-in-out;
+            -moz-transition: all 0.4s ease-in-out;
+            -o-transition: all 0.4s ease-in-out;
+            transition: all 0.4s ease-in-out;
+        }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import Router from "@/router/index";
+import { createApp } from 'vue'
+import App from './App.vue'
+import Router from '@/router/index'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faSun, faMoon)
 
-const app = createApp(App);
-app.use(Router);
+const app = createApp(App)
+app.use(Router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
-app.mount("#app");
+app.mount('#app')
