5013176215330027154dbb4cafd51d2a888cc939
junyoung <cjy874545@gmail.com>
2021-07-22 09:50:27 +0900

    update

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import Router from '@/router/index'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSun, faMoon, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faHome, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faSun, faMoon, faHome)
+library.add(faSun, faMoon, faHome, faChevronRight)
 
 const app = createApp(App)
 app.use(Router)
