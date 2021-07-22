1da4fa4c7c0d18b4f0a85851bc9ad1d6127d6d0b
junyoung <cjy874545@gmail.com>
2021-07-22 09:51:46 +0900

    update

--- src/components/AppMenuItem.vue
+++ src/components/AppMenuItem.vue
@@ -1,25 +1,25 @@
 <template>
-    <div id="menuItemContainer" class="flex flex-col justify-center align-center">
-        <slot name="button"></slot>
-        <font-awesome-icon icon="chevron-right" class="chevron-right-icon"></font-awesome-icon>
-    </div>
+  <div id="menuItemContainer" class="flex flex-col justify-center align-center">
+    <slot name="button"></slot>
+    <font-awesome-icon icon="chevron-right" class="chevron-right-icon"></font-awesome-icon>
+  </div>
 </template>
 <script setup></script>
 <style lang="scss" scoped>
 #menuItemContainer {
-    width: 100%;
-    min-width: 50px;
-    height: auto;
-    max-height: 100px;
-    font-size: 1.3rem;
-    padding: 10px 20px;
-    position: relative;
-    & .chevron-right-icon {
-        position: absolute;
-        right: 20px;
-        top: 50%;
-        color: var(--default-color);
-        transform: translateY(-50%);
-    }
+  width: 100%;
+  min-width: 50px;
+  height: auto;
+  max-height: 100px;
+  font-size: 1.3rem;
+  padding: 10px 20px;
+  position: relative;
+  & .chevron-right-icon {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    color: var(--default-color);
+    transform: translateY(-50%);
+  }
 }
 </style>
