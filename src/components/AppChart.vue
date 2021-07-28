cef36db3269c6d7294f9217c96c5057368b683c7
junyoung <cjy874545@gamil.com>
2021-07-28 23:57:04 +0900

    chart 추가

--- src/components/AppChart.vue
+++ src/components/AppChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas id="chartBar" width="400" height="400"></canvas>
+  <canvas :id="chartId"></canvas>
 </template>
 <script>
 import { defineComponent, onMounted, toRefs, watch } from 'vue'
@@ -7,59 +7,24 @@ import { defineComponent, onMounted, toRefs, watch } from 'vue'
 import { Chart } from 'chart.js'
 export default defineComponent({
   props: {
-    chartData: Array,
-    labels: Array,
+    config: {},
+    chartData: {},
+    chartId: String,
   },
   setup(props) {
-    const { chartData, labels } = toRefs(props)
+    const { chartId, config, chartData } = toRefs(props)
+    const chartConfig = config.value
 
-    let chartBar
+    let chart
     const loadChart = () => {
-      const ctx = document.querySelector('#chartBar')
-      chartBar = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: labels.value,
-          datasets: [
-            {
-              label: '# of Votes',
-              data: chartData.value,
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(28, 224, 255, 0.2)',
-                'rgba(255, 28, 125, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(28, 224, 255, 1)',
-                'rgba(255, 28, 125, 1)',
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
+      const ctx = document.querySelector(`#${chartId.value}`)
+      chart = new Chart(ctx, {
+        ...chartConfig,
       })
     }
     const setChartInfo = () => {
-      chartBar.data.datasets[0].data = chartData.value
-      chartBar.update()
+      chart.data.datasets[0].data = chartData.value
+      chart.update()
     }
     watch(chartData, () => {
       setChartInfo()
@@ -67,6 +32,9 @@ export default defineComponent({
     onMounted(() => {
       loadChart()
     })
+    return {
+      chartId,
+    }
   },
 })
 </script>
--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="dashboardContainer">
-    
-    <div class="chart-bar__container">
-      <AppChart :chartData="chartData.value" :labels="['치킨', '피자', '햄버거', '라면', '자장면', '돈가스', '국밥', '된장찌개']" />
+    <div class="chart-container__wrap">
+      <div class="chart__container">
+        <AppChart chartId="chartBar" :config="barCartConfig" :chartData="barChartInitData.value" />
+      </div>
+    </div>
+
+    <div class="chart-container__wrap">
+      <div class="chart__container">
+        <AppChart chartId="chartLine" :config="lineCartConfig" :chartData="lineChartInitData.value" />
+      </div>
     </div>
   </div>
 </template>
@@ -13,38 +20,168 @@ const AppChart = defineAsyncComponent({
   loadingComponent: inject('LoadingComponent'),
   errorComponent: inject('ErrorComponent'),
 })
-const chartData = reactive({
-  value: [
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-  ],
-})
-setInterval(() => {
-  chartData.value = [
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-    Math.random() * 10,
-  ]
-}, 5000)
+
+const barChart = () => {
+  const barChartInitData = reactive({
+    value: [
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+    ],
+  })
+  const barCartConfig = {
+    type: 'bar',
+    data: {
+      labels: ['치킨', '피자', '햄버거', '라면', '자장면', '돈가스', '국밥', '된장찌개'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: barChartInitData.value,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(28, 224, 255, 0.2)',
+            'rgba(255, 28, 125, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(28, 224, 255, 1)',
+            'rgba(255, 28, 125, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  }
+
+  setInterval(() => {
+    barChartInitData.value = [
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+    ]
+  }, 5000)
+
+  return {
+    barCartConfig,
+    barChartInitData,
+  }
+}
+
+const lineChart = () => {
+  const lineChartInitData = reactive({
+    value: [
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+    ],
+  })
+  const lineCartConfig = {
+    type: 'line',
+    data: {
+      labels: ['치킨', '피자', '햄버거', '라면', '자장면', '돈가스', '국밥', '된장찌개'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: lineChartInitData.value,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(28, 224, 255, 0.2)',
+            'rgba(255, 28, 125, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(28, 224, 255, 1)',
+            'rgba(255, 28, 125, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  }
+
+  setInterval(() => {
+    lineChartInitData.value = [
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+      Math.random() * 1000,
+    ]
+  }, 5000)
+
+  return {
+    lineCartConfig,
+    lineChartInitData,
+  }
+}
+
+const { barCartConfig, barChartInitData } = barChart()
+const { lineCartConfig, lineChartInitData } = lineChart()
 </script>
 <style lang="scss" scoped>
 #dashboardContainer {
   width: 100%;
-  .chart-bar__container {
-    width: 400px;
-    height: 400px;
-    border: 1px solid var(--default-color);
+  .chart-container__wrap {
+    padding: 4px;
+    & .chart__container {
+      width: 500px;
+      height: 300px;
+      border: 1px solid var(--default-color);
+      border-radius: 10px;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    }
   }
 }
 </style>
